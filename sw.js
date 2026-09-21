7d61002b0ebee2eaa0dbb56392b3d0e899908847
asimrbrohi-boss <asimrbrohi@gmail.com>
2026-09-21 15:46:17 -0400

    Add service worker for app shell caching

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
+const CACHE = 'quran-reader-v1';
+const APP_SHELL = ['./', './index.html', './manifest.json'];
 
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      if (event.request.url.startsWith(self.location.origin)) {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
